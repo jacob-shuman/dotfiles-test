dbb5113c458cacdf2c13d2ec6c1f9f78cecca8e6
Jacob Shuman <44483276+jacob-shuman@users.noreply.github.com>
2024-01-05 16:23:05 -0500

    Update .wezterm.lua Update install_homebrew.sh

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -5,6 +5,17 @@ local wezterm = require 'wezterm'
 local config = {}
 
 
+
+
+
+
+
+
+
+
+
+
+
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
