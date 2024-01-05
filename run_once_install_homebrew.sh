d398e8297a6df46a1fd18f733c78a3cc48efdc13
Jacob Shuman <44483276+jacob-shuman@users.noreply.github.com>
2024-01-05 15:47:27 -0500

    Create run_once_install_homebrew.sh

---
+++ run_once_install_homebrew.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# install homebrew
+if ! command -v brew &> /dev/null; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+echo $(pwd)
