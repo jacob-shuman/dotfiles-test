c291cf5c4bb0b46cc53ba207141fabe4dfe664d8
Jacob Shuman <44483276+jacob-shuman@users.noreply.github.com>
2024-01-09 17:34:50 -0500

    Update .wezterm.lua

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -1,8 +1,7 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
--- config.color_scheme = 'Catppuccin Mocha'
-config.color_scheme = 'ayu'
+config.color_scheme = 'Catppuccin Mocha'
 
 -- window
 config.window_decorations = "RESIZE"
