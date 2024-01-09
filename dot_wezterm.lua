7f5c07d062882f85f4000a6fb1c3656142087ee4
Jacob Shuman <44483276+jacob-shuman@users.noreply.github.com>
2024-01-09 12:01:06 -0500

    Update .wezterm.lua

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -4,6 +4,7 @@ local wezterm = require 'wezterm'
 -- This table will hold the configuration.
 -- local config = {}
 
+
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 -- if wezterm.config_builder then
