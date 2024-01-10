427967b7c5a0d5b9837f9c8497c86539e5160c66
Jacob Shuman <44483276+jacob-shuman@users.noreply.github.com>
2024-01-09 21:53:37 -0500

    Update .wezterm.lua

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -10,6 +10,7 @@ config.color_scheme = 'Tokyo Night'
 config.window_decorations = "RESIZE"
 config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
+config.inactive_pane_hsb = { saturation = 0.8, brightness = 0.7 }
 
 -- font
 config.font = wezterm.font 'Monocraft'
