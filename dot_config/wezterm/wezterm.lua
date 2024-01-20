88c291b8c2a85dab16b58d029d0002e058312f94
Jacob Shuman <44483276+jacob-shuman@users.noreply.github.com>
2024-01-20 12:41:53 -0500

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 -- config.color_scheme = 'Tokyo Night'
-config.color_scheme = 'Afterglow'
+config.color_scheme = 'Atelier Plateau (base16)'
 
 -- window
 config.window_decorations = "RESIZE"
