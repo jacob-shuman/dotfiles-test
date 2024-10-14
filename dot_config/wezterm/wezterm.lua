1b38225044a6d22e8136d46197406a5a622a5a05
Jacob Shuman <44483276+jacob-shuman@users.noreply.github.com>
2024-10-14 18:16:45 -0400

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,8 +1,6 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
---config.color_scheme = 'Tokyo Night'
---config.color_scheme = 'Ayu Dark (Gogh)'
 config.color_scheme = 'Ayu Dark (Gogh)'
 
 -- window
