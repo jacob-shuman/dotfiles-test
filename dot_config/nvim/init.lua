a962869c9881de82a73751d6a050567576b474b2
Jacob Shuman <44483276+jacob-shuman@users.noreply.github.com>
2024-10-14 23:20:27 -0400

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -25,7 +25,7 @@ local plugins = {
 		'goolord/alpha-nvim',
 		dependencies = { 'echasnovski/mini.icons', 'nvim-lua/plenary.nvim' },
 		config = function ()
-			require'alpha'.setup(require'alpha.themes.theta'.config)
+			require'alpha'.setup(require'alpha.themes.dashboard'.config)
 		end
 	},
 	{
