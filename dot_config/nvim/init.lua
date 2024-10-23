24a55035e18e09110f830f212a34bf7d7ae04f12
Jacob Shuman <44483276+jacob-shuman@users.noreply.github.com>
2024-10-23 15:52:16 -0400

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -52,7 +52,7 @@ local plugins = {
 			local configs = require("nvim-treesitter.configs")
 
 			configs.setup({
-				ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+				ensure_installed = { "lua", "javascript", "html", "css", "typescript", "svelte", "markdown" },
 				sync_install = false,
 				highlight = { enable = true },
 				indent = { enable = true },  
