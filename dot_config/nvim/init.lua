875da046213ca913dcbd5e797e4c13af50eab09e
Jacob Shuman <44483276+jacob-shuman@users.noreply.github.com>
2024-10-23 16:09:55 -0400

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -44,6 +44,11 @@ local plugins = {
 	{
 		'stevearc/conform.nvim',
 		opts = {},
+		formatters_by_ft = {
+			lua = { "stylua" },
+			-- Conform will run the first available formatter
+			javascript = { "prettierd", "prettier", stop_after_first = true },
+		},
 	},
 	{
 		"nvim-treesitter/nvim-treesitter",
