23cb1366889ee1b8f1159850a991e1ed9c34dbef
Jacob Shuman <44483276+jacob-shuman@users.noreply.github.com>
2024-10-23 15:47:55 -0400

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -41,7 +41,24 @@ local plugins = {
 			},
 		},
 	},
-	"nvim-treesitter/nvim-treesitter",
+	{
+		'stevearc/conform.nvim',
+		opts = {},
+	}
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function () 
+			local configs = require("nvim-treesitter.configs")
+
+			configs.setup({
+				ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+				sync_install = false,
+				highlight = { enable = true },
+				indent = { enable = true },  
+			})
+		end
+	},
 	{
 		'Shatur/neovim-ayu',
 		lazy = false,
