6873b2cecad768ef5addd7f458a382162c612a94
Jacob Shuman <44483276+jacob-shuman@users.noreply.github.com>
2024-10-23 21:04:29 -0400

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -116,7 +116,7 @@ vim.cmd.colorscheme "ayu"
 -- custom line number colors
 vim.opt.cursorline = true
 vim.api.nvim_set_hl(0, "LineNr", { fg = "#3D4149" })  
-vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#646974" }) 
+vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#F09552" }) 
 
 -- tab width
 vim.opt.tabstop = 4
