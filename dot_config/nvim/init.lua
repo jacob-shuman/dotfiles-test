a4f23bffee4675245387209a80da1acc34b8c0a7
Jacob Shuman <44483276+jacob-shuman@users.noreply.github.com>
2024-10-23 21:06:40 -0400

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -55,7 +55,11 @@ local plugins = {
 		config = function()
 			local conform = require('conform')
 			conform.setup({
-				
+			format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_format = "fallback",
+  },	
 		formatters_by_ft = {
 			lua = { "stylua" },
 			javascript = { { "prettierd", "prettier" } },
