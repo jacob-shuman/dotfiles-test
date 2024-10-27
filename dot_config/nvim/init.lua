0241c5991b67d009a9a9a9b59659ababb86b0fff
Jacob Shuman <44483276+jacob-shuman@users.noreply.github.com>
2024-10-26 21:18:26 -0400

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,10 +1,4 @@
--- leader
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-
--- line numbers
-vim.wo.relativenumber = true
-vim.wo.number = true
+require("settings.lua")
 
 -- bootstrap lazy nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -118,43 +112,6 @@ local plugins = {
 
 require("lazy").setup(plugins, {})
 
--- sync nvim and system clipboard
-vim.opt.clipboard = "unnamedplus"
-
--- indent on line wrap
-vim.opt.breakindent = true
-
--- save undo history
-vim.opt.undofile = true
-
--- case insensitive search (unless \C or one or more capital letters are in the search term
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-
--- keep signcolumn on by default
-vim.opt.signcolumn = "yes"
-
--- decrease update time
-vim.opt.updatetime = 250
-
--- decrease mapped sequence wait time (displays which-key popup sooner)
-vim.opt.timeoutlen = 300
-
--- theme
-vim.cmd.colorscheme("ayu")
-vim.g.have_nerd_font = true
-vim.opt.showmode = false
-
--- custom line number colors
-vim.opt.cursorline = true
-vim.api.nvim_set_hl(0, "LineNr", { fg = "#3D4149" })
-vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#F09552" })
-
--- tab width
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-
 -- noice
 require("noice").setup({
 	lsp = {
@@ -233,11 +190,3 @@ require("lualine").setup({
 		theme = "ayu",
 	},
 })
-
--- files without extensions use bash
-vim.cmd([[
-  augroup set_bash_for_no_extension
-    autocmd!
-    autocmd BufNewFile,BufRead * if expand("%:e") == "" | setlocal filetype=bash | endif
-  augroup END
-]])
