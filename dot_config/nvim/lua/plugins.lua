5e90fc9e7e74410dbe54ff99470c6a822c90dc6c
Jacob Shuman <44483276+jacob-shuman@users.noreply.github.com>
2024-10-26 23:43:49 -0400

    Add .config/nvim/lua/plugins.lua Add .config/nvim/lua/remap.lua Add .config/nvim/lua/settings.lua

---
+++ dot_config/nvim/lua/plugins.lua
@@ -0,0 +1,179 @@
+-- bootstrap lazy nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {
+	{
+		"goolord/alpha-nvim",
+		dependencies = { "echasnovski/mini.icons" },
+		config = function()
+			require("alpha").setup(require("alpha.themes.startify").config)
+		end,
+	},
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		keys = {
+			{
+				"<leader>?",
+				function()
+					require("which-key").show({ global = false })
+				end,
+				desc = "Buffer Local Keymaps (which-key)",
+			},
+		},
+	},
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		dependencies = {
+			"MunifTanjim/nui.nvim",
+			"rcarriga/nvim-notify",
+		},
+	},
+	{
+		"stevearc/conform.nvim",
+		event = { "BufReadPre", "BufNewFile	" },
+		config = function()
+			local conform = require("conform")
+			conform.setup({
+				format_on_save = {
+					timeout_ms = 500,
+					lsp_format = "fallback",
+					stop_after_first = true,
+				},
+				formatters_by_ft = {
+					lua = { "stylua" },
+					javascript = { "prettierd", "prettier" },
+					typescript = { "prettierd", "prettier" },
+					svelte = { "prettierd", "prettier" },
+				},
+			})
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local configs = require("nvim-treesitter.configs")
+
+			configs.setup({
+				ensure_installed = { "lua", "javascript", "html", "css", "typescript", "svelte", "markdown" },
+				sync_install = false,
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
+	},
+	{
+		"Shatur/neovim-ayu",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("ayu").setup({
+				mirage = false,
+				terminal = true,
+				overrides = {},
+			})
+		end,
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		branch = "0.1.x",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
+	"nvim-tree/nvim-web-devicons",
+	{
+		"nvim-telescope/telescope-file-browser.nvim",
+		dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons", opt = true },
+	},
+	{
+		"folke/todo-comments.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = {},
+	},
+}
+
+require("lazy").setup(plugins, {})
+
+
+-- noice
+require("noice").setup({
+	lsp = {
+		-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+		override = {
+			["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+			["vim.lsp.util.stylize_markdown"] = true,
+			["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+		},
+	},
+	-- you can enable a preset for easier configuration
+	presets = {
+		bottom_search = true, -- use a classic bottom cmdline for search
+		-- command_palette = true, -- position the cmdline and popupmenu together
+		long_message_to_split = true, -- long messages will be sent to a split
+		inc_rename = false, -- enables an input dialog for inc-rename.nvim
+		lsp_doc_border = false, -- add a border to hover docs and signature help
+	},
+	command_palette = {
+		popupmenu = {
+			relative = "editor",
+			position = {
+				row = 23,
+				col = "50%",
+			},
+			size = {
+				width = 60,
+				height = "auto",
+				max_height = 15,
+			},
+			border = {
+				style = "rounded",
+				padding = { 0, 1 },
+			},
+			win_options = {
+				winhighlight = { Normal = "Normal", FloatBorder = "NoiceCmdlinePopupBorder" },
+			},
+		},
+	},
+})
+
+-- telescope
+local actions = require("telescope.actions")
+
+
+require("telescope").setup({
+	defaults = {
+		mappings = {
+			i = {
+				["<C-j>"] = actions.move_selection_next, -- Go to the next item
+				["<C-k>"] = actions.move_selection_previous, -- Go to the previous item
+			},
+		},
+	},
+})
+
+-- require("telescope").load_extension "file_browser"
+
+require("nvim-web-devicons").setup()
+
+-- lualine
+require("lualine").setup({
+	options = {
+		theme = "ayu",
+	},
+})
