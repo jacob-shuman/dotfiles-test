17872fb17572e9399b3923c3cee93c2426ead498
Jacob Shuman <44483276+jacob-shuman@users.noreply.github.com>
2024-10-28 00:36:06 -0400

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,7 +1,7 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-config.color_scheme = 'Ayu Dark (Gogh)'
+config.color_scheme = "Ayu Dark (Gogh)"
 
 -- window
 config.window_decorations = "RESIZE"
@@ -10,27 +10,31 @@ config.hide_tab_bar_if_only_one_tab = true
 config.inactive_pane_hsb = { saturation = 0.8, brightness = 0.7 }
 
 -- font
-config.font = wezterm.font 'Monocraft Nerd Font'
+config.font = wezterm.font("Monocraft Nerd Font")
 config.font_size = 16.0
 
 -- multiplexing
-config.leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 1000 }
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
 config.keys = {
-  {
-    mods   = "LEADER",
-    key    = "-",
-    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' }
-  },
-  {
-    mods   = "LEADER",
-    key    = "=",
-    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' }
-  },
-  {
-    mods = 'LEADER',
-    key = 'm',
-    action = wezterm.action.TogglePaneZoomState
-  },
+	{
+		mods = "LEADER",
+		key = "-",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		mods = "LEADER",
+		key = "=",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		mods = "LEADER",
+		key = "m",
+		action = wezterm.action.TogglePaneZoomState,
+	},
+	{
+		key = "CapsLock",
+		action = wezterm.action.SendKey({ key = "Escape" }),
+	},
 }
 
 return config
