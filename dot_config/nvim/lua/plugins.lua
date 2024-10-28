ba5256d6f9070c95f3b1547a74553afc253d3ee7
Jacob Shuman <44483276+jacob-shuman@users.noreply.github.com>
2024-10-28 17:46:58 -0400

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -139,6 +139,17 @@ local plugins = {
 	},
 	-- NOTE: file tree
 
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+			"MunifTanjim/nui.nvim",
+			-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+		},
+	},
+
 	-- NOTE: comments
 
 	-- uses correct comment syntax in embedded languages (svelte, vue, etc)
