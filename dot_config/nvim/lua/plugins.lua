1cb847926dc9396b7aca0cbb341596020cda95f9
Jacob Shuman <44483276+jacob-shuman@users.noreply.github.com>
2024-10-28 01:37:39 -0400

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -158,10 +158,10 @@ local plugins = {
 					lua = "rainbow-blocks",
 				},
 				highlight = {
+					"RainbowDelimiterOrange",
+					"RainbowDelimiterBlue",
 					"RainbowDelimiterRed",
 					"RainbowDelimiterYellow",
-					"RainbowDelimiterBlue",
-					"RainbowDelimiterOrange",
 					"RainbowDelimiterGreen",
 					"RainbowDelimiterViolet",
 					"RainbowDelimiterCyan",
@@ -233,11 +233,6 @@ require("telescope").setup({
 			},
 		},
 	},
-	--	extensions = {
-	--		persisted = {
-	--			layout_config = { width = 0.55, height = 0.55 },
-	--		},
-	--	},
 })
 
 -- require("telescope").load_extension("persisted")
@@ -252,10 +247,10 @@ require("Comment").setup()
 
 -- indent-blankline.nvim
 local highlight = {
+	"RainbowOrange",
+	"RainbowBlue",
 	"RainbowRed",
 	"RainbowYellow",
-	"RainbowBlue",
-	"RainbowOrange",
 	"RainbowGreen",
 	"RainbowViolet",
 	"RainbowCyan",
@@ -264,10 +259,10 @@ local hooks = require("ibl.hooks")
 -- create the highlight groups in the highlight setup hook, so they are reset
 -- every time the colorscheme changes
 hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+	vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+	vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
 	vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
 	vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
-	vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
-	vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
 	vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
 	vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
 	vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
@@ -277,5 +272,3 @@ vim.g.rainbow_delimiters = { highlight = highlight }
 require("ibl").setup({ scope = { highlight = highlight } })
 
 hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
-
--- require("ibl").setup()
