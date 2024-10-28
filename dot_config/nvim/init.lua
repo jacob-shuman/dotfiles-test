92164a33034a86ea88b7ce0f99657fba52eb73cb
Jacob Shuman <44483276+jacob-shuman@users.noreply.github.com>
2024-10-28 01:58:06 -0400

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,6 +1,6 @@
 -- show whitespace characters
 vim.opt.list = true
--- vim.opt.listchars = { tab = "▸ ", trail = "·", nbsp = "␣" }
+vim.opt.listchars = { tab = "▸ ", trail = "·", nbsp = "␣" }
 
 require("settings")
 require("plugins")
