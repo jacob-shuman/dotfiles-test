aeabf0fbe5ebed41bfa2be8ce65ca3d1917280a4
Jacob Shuman <44483276+jacob-shuman@users.noreply.github.com>
2024-10-28 17:32:27 -0400

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -115,19 +115,19 @@ local plugins = {
 		dependencies = { "nvim-lua/plenary.nvim" },
 	},
 	{
-  "danielfalk/smart-open.nvim",
-  branch = "0.2.x",
-  config = function()
-    require("telescope").load_extension("smart_open")
-  end,
-  dependencies = {
-    "kkharji/sqlite.lua",
-    -- Only required if using match_algorithm fzf
-    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-    -- Optional.  If installed, native fzy will be used when match_algorithm is fzy
-    { "nvim-telescope/telescope-fzy-native.nvim" },
-  },
-},
+		"danielfalk/smart-open.nvim",
+		branch = "0.2.x",
+		config = function()
+			require("telescope").load_extension("smart_open")
+		end,
+		dependencies = {
+			"kkharji/sqlite.lua",
+			-- Only required if using match_algorithm fzf
+			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+			-- Optional.  If installed, native fzy will be used when match_algorithm is fzy
+			{ "nvim-telescope/telescope-fzy-native.nvim" },
+		},
+	},
 	"nvim-tree/nvim-web-devicons",
 	{
 		"nvim-telescope/telescope-file-browser.nvim",
