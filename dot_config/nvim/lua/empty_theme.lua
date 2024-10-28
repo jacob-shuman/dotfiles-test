3c1f568bb495da515b6eb8b3884fd8b9e808e517
Jacob Shuman <44483276+jacob-shuman@users.noreply.github.com>
2024-10-28 02:04:14 -0400

    Update .config/nvim/lua/theme.lua

--- dot_config/nvim/lua/empty_theme.lua
+++ dot_config/nvim/lua/empty_theme.lua
@@ -10,3 +10,7 @@ vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#F09552" })
 -- whitespace color
 vim.api.nvim_set_hl(0, "Whitespace", { fg = "#5c6370" })
 vim.api.nvim_set_hl(0, "SpecialKey", { fg = "#5c6370" })
+
+vim.api.nvim_set_hl(0, "Whitespace", { fg = "#FFD700" }) -- Gold
+vim.api.nvim_set_hl(0, "SpecialKey", { fg = "#FFD700" }) -- Gold
+vim.api.nvim_set_hl(0, "NonText", { fg = "#FFD700" }) -- Gold
