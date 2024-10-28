c91729edbd4e5bfa17987a5efefdd352278f7608
Jacob Shuman <44483276+jacob-shuman@users.noreply.github.com>
2024-10-28 02:00:30 -0400

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,9 +1,8 @@
 -- show whitespace characters
 vim.opt.list = true
 vim.opt.listchars = { tab = "▸ ", trail = "·", nbsp = "␣" }
-
-					
-
+vim.api.nvim_set_hl(0, "Whitespace", { fg = "#5c6370" })
+vim.api.nvim_set_hl(0, "SpecialKey", { fg = "#5c6370" })
 
 require("settings")
 require("plugins")
