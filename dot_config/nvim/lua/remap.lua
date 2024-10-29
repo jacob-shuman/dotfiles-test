8e194840e474a54614d2aa2eefbc676593ecf78a
Jacob Shuman <44483276+jacob-shuman@users.noreply.github.com>
2024-10-28 22:19:56 -0400

    Update .config/nvim/lua/remap.lua

--- dot_config/nvim/lua/remap.lua
+++ dot_config/nvim/lua/remap.lua
@@ -18,7 +18,12 @@ vim.api.nvim_set_keymap(
 
 -- NOTE: ferm.vim
 -- Open fern in a vertical split with Ctrl-n
-vim.api.nvim_set_keymap("n", "<C-n>", ":Fern . -reveal=% -drawer -width=30<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap(
+	"n",
+	"<C-n>",
+	":Fern . -reveal=% -drawer -toggle -right -width=30<CR>",
+	{ noremap = true, silent = true }
+)
 
 -- NOTE: todo comments
 vim.keymap.set("n", "]t", function()
