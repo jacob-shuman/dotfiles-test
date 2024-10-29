c4defdf988b73b18435b64cd2e5a8f71a37acd6b
Jacob Shuman <44483276+jacob-shuman@users.noreply.github.com>
2024-10-29 19:48:15 -0400

    Update .config/nvim/lua/remap.lua

--- dot_config/nvim/lua/remap.lua
+++ dot_config/nvim/lua/remap.lua
@@ -24,7 +24,7 @@ vim.api.nvim_set_keymap(
 vim.api.nvim_set_keymap(
 	"n",
 	"<C-n>",
-	":Fern . -reveal=% -toggle -right -width=30<CR>",
+	":Fern . -reveal=% -toggle -drawer -right -width=30<CR>",
 	-- ":Fern . -reveal=% -drawer -toggle -right -width=30<CR>",
 	{ noremap = true, silent = true }
 )
