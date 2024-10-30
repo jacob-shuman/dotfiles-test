03d3344b5aaf6f00f82aacc2610a91797f85ae1a
Jacob Shuman <44483276+jacob-shuman@users.noreply.github.com>
2024-10-30 08:37:55 -0400

    Update .config/nvim/lua/remap.lua

--- dot_config/nvim/lua/remap.lua
+++ dot_config/nvim/lua/remap.lua
@@ -58,5 +58,6 @@ local function yank_comment_paste()
 	end
 end
 
-vim.api.nvim_set_keymap("n", "<leader>yc", ":lua yank_comment_paste()<CR>", { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap("n", "<leader>yc", ":lua yank_comment_paste()<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>yc", yank_comment_paste(), { noremap = true, silent = true })
 vim.api.nvim_set_keymap("v", "<leader>yc", ":<C-u>lua yank_comment_paste()<CR>", { noremap = true, silent = true })
